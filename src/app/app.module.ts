69ca224b41b269a5bc05a506d40a68ed2864d5a8
Joe <joe@s2h.ca>
2021-06-12 12:56:09 +0530

    property and event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {Greeting} from './../greeting/greeting.component'
+import {EmployeeList} from './../employeeList/employeeList.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent,Greeting
+    AppComponent,EmployeeList
   ],
   imports: [
     BrowserModule
